eb46699702fd0d82230d76b2f28e11b80646fbce
Mark Fruman <mark.fruman@yahoo.com>
2016-05-25 00:00:52 -0400

    starting a new module for PCA computation

---
+++ Python/PBNPCA/pca.py
@@ -0,0 +1,6 @@
+def jpg_to_hsv(img):
+    # convert a jpg image (in form of a PIL.Image object) to a list
+    # of three lists of HSV (hue-saturation-value) data
+    #
+    hsv = None
+    return hsv
--- Python/PBNPCA/setup.py
+++ Python/PBNPCA/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='readPBNData',
+setup(name='PBNPCA',
         version='0.1',
-        description='Read and preprocess data for Painter By Numbers kaggle',
+        description='Generate and use principal components on image data',
         url='http://github.com/majorgowan/painterByNumbers_5thFloor',
         author='Mark Fruman',
         author_email='majorgowan@yahoo.com',
         license='MIT',
         packages=['description'],
-        install_requires=[],
+        install_requires=['numpy'],
         zip_safe=False)
