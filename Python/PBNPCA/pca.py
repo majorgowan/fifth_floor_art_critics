2b701d407c927b504d3634592e91b77410dfbfe9
Mark Fruman <majorgowan@yahoo.com>
2016-05-27 01:24:26 -0400

    routine to compute PCA (eigenvectors of scatter matrix) ... next: project onto PC and logistic regression!

--- Python/PBNPCA/pca.py
+++ Python/PBNPCA/pca.py
@@ -1,9 +1,33 @@
-def jpg_to_hsv(img):
-    # convert a jpg image (in form of a PIL.Image object) to a list
-    # of three lists of HSV (hue-saturation-value) data
+def pca(data, ncomp):
+    # given a list of lists of features, compute the
+    # leading principal components (PC)
     #
-    import colorsys
-    rgblist = list(img.getdata())
-    rgblist = [(1.0*r/256,1.0*g/256,1.0*b/256) for (r,g,b) in rgblist]
-    hsvlist = [colorsys.rgb_to_hsv(*rgb) for rgb in rgblist]
-    return hsvlist
+    # The leading PC is the vector in the direction in
+    # which the data varies maximally with length equal
+    # to the variance along the axis
+    #
+    # The next PC is the direction in which the data
+    # projected onto the complement of the first PC
+    # varies maximally ... and so on
+    #
+    import numpy as np
+    import scipy.linalg as spla
+    ndim = len(data[0])
+    data_mat = np.array(data)
+    # compute mean vector
+    meanvec = [np.mean(data[:,i]) for i in xrange(len(data[0,:]))]
+    datadiff = np.array([d-meanvec for d in data])
+    # scatter matrix
+    scat = datadiff.transpose().dot(datadiff)
+    # leading eigenvectors and eigenvalues of scatter matrix
+    eigvals, eigvecs = spla.eigh(scat,eigvals=(ndim-ncomp,ndim-1))
+    variances = [np.sqrt(e/ndim) for e in eigvals]
+    # construct pca object
+    pcaObj = {
+            'ncomp':   ncomp,
+            'meanvec': meanvec,
+            'eigvecs': eigvecs,
+            'eigvals': eigvals,
+            'variances': variances
+            }
+    return pcaObj
